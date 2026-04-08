66e9052555b600009f729322822808920220a420
Amit Singh <singh.amitk82@gmail.com>
2026-04-09 00:14:57 +0530

    feat: Support purely base64 encoded firebase admin configs dynamically

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -3,8 +3,13 @@ const admin = require("firebase-admin");
 let serviceAccount;
 
 try {
-    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-    serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+    if (process.env.FIREBASE_SERVICE_ACCOUNT_BASE64) {
+        const decoded = Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString('utf8');
+        serviceAccount = JSON.parse(decoded);
+    } else {
+        serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+        serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+    }
 } catch (err) {
     console.error("🔥 FIREBASE CONFIG ERROR:", err.message);
 }
